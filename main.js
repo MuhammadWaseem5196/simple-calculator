916c7551b269a9c2d1e718e7f8fb2f4ad3699c70
MuhammadWaseem5196 <m.waseem5196@gmail.com>
2024-03-29 12:58:06 +0500

    my first calculator

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter your first Number", type: "number", name: "Firstnumber" },
+    { message: "Enter your Second Number", type: "number", name: "secondnumber" },
+    { message: "Enter your operatos", type: "list", name: "operator", choices: ['addition', 'subtraction', 'multipliaction', 'divison'] }]);
+if (answer.operator === "addition") {
+    console.log(answer.Firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.Firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multipliaction") {
+    console.log(answer.Firstnumber * answer.secondnumber);
+}
+else {
+    console.log(answer.Firstnumber / answer.secondnumber);
+}
