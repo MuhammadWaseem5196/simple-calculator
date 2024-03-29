73c0e06705f3e17ecb845ca1826f2de6be398332
MuhammadWaseem5196 <m.waseem5196@gmail.com>
2024-03-29 13:17:10 +0500

    we add some updates in this verson

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{ message: "Enter your first Number", type: "number", name: "Firstnumber" },
-    { message: "Enter your Second Number", type: "number", name: "secondnumber" },
-    { message: "Enter your operatos", type: "list", name: "operator", choices: ['addition', 'subtraction', 'multipliaction', 'divison'] }]);
+const answer = await inquirer.prompt([{ message: "Enter  first Number", type: "number", name: "Firstnumber" },
+    { message: "Enter  Second Number", type: "number", name: "secondnumber" },
+    { message: "Enter  operatos", type: "list", name: "operator", choices: ['addition', 'subtraction', 'multipliaction', 'divison'] }]);
 if (answer.operator === "addition") {
     console.log(answer.Firstnumber + answer.secondnumber);
 }
--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@
 
 import inquirer from "inquirer";
 
-const answer =await inquirer.prompt([{message:"Enter your first Number",type:"number", name:"Firstnumber" },
-{message:"Enter your Second Number",type:"number", name:"secondnumber" },
-{message:"Enter your operatos",type:"list", name:"operator",choices:['addition','subtraction','multipliaction','divison'] }]);
+const answer =await inquirer.prompt([{message:"Enter  first Number",type:"number", name:"Firstnumber" },
+{message:"Enter  Second Number",type:"number", name:"secondnumber" },
+{message:"Enter  operatos",type:"list", name:"operator",choices:['addition','subtraction','multipliaction','divison'] }]);
 
 if(answer.operator === "addition"){
     console.log(answer.Firstnumber + answer.secondnumber);
